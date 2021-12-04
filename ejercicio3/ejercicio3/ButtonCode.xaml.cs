2bccc760deea81a3277e7da8c167d8e8e33b2f32
smith <smith18200@gmail.com>
2021-12-04 00:09:25 -0500

    Agregar archivos de proyecto.

---
+++ ejercicio3/ejercicio3/ButtonCode.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ejercicio3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ButtonCode : ContentPage
+    {
+        public ButtonCode()
+        {
+            InitializeComponent();
+
+            Title = "code button click";
+            Label label = new Label();
+            {
+               // Text = "click the button below",
+               // FontSize = Device.GetNamedSize(NamedSize.Large,typeof(Label)),
+              //  VerticalOptions = LayoutOptions.CenterAndExpand,
+               // HorizontalOptions = LayoutOptions.Center
+
+            }
+        }
+    }
+}
---
+++ ejercicio3/ejercicio3/ButtonDemo.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ejercicio3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ButtonDemo : ContentPage
+    {
+        public ButtonDemo()
+        {
+            InitializeComponent();
+        }
+
+        async void onbuttonclicked(object sender, EventArgs e)
+        {
+            await label.RelRotateTo(360, 1000);
+        }
+    }
+}
